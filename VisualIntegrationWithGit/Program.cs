1ae016b441795bce6d9cc9b4eef38c05e346e85b
Bantupalli Venkateswararao <bantupalli.venkateswararao@capgemini.com>
2021-04-01 07:42:18 +0530

    Btanch02 commit message

--- VisualIntegrationWithGit/Program.cs
+++ VisualIntegrationWithGit/Program.cs
@@ -6,7 +6,7 @@ namespace VisualIntegrationWithGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World02!");
         }
     }
 }
