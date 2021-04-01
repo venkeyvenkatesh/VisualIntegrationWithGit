06ff05813ad4f34792b5ac65758626d64a52fc10
Bantupalli Venkateswararao <bantupalli.venkateswararao@capgemini.com>
2021-04-01 08:03:58 +0530

    Bvds Commit

--- VisualIntegrationWithGit/Program.cs
+++ VisualIntegrationWithGit/Program.cs
@@ -6,7 +6,7 @@ namespace VisualIntegrationWithGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello bcds");
+            Console.WriteLine("Hello bvds");
         }
     }
 }
