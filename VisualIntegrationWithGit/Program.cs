04473277a3a78ae78b4293b47e3fb4a0fddf8e44
Bantupalli Venkateswararao <bantupalli.venkateswararao@capgemini.com>
2021-04-01 07:34:21 +0530

    Add project files.

---
+++ VisualIntegrationWithGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualIntegrationWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
